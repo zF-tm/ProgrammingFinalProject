54ac5ae53ec2d31ae3c3b508ca87f1a896827d01
zF-tm <73698972+zF-tm@users.noreply.github.com>
2026-01-13 19:17:54 +0300

    Add files via upload

--- EventDriven/src/EventDriven/EventDrivenMain.java
+++ EventDriven/src/EventDriven/EventDrivenMain.java
@@ -11,6 +11,11 @@ public class EventDrivenMain {
 	public static String locationn = "";
 	public static int datee = 0;
 	public static String durationn = "";
+	
+	public static String _participantName = "";
+	public static String _participantContactInto = "";
+	public static int _eventID = 0;
+	
 	public static void main(String[] args) {
 		
 		EventManager manager = new EventManager();
@@ -510,6 +515,314 @@ public class EventDrivenMain {
         	}
         });
         
+        
+        button5.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		JFrame addParticipantFrame = new JFrame("Add Participant");
+        		addParticipantFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+        		addParticipantFrame.setSize(400, 300);
+        		addParticipantFrame.setLayout(new GridLayout(7,2,10,10));
+                
+        		
+        		
+        		JLabel eventIDInput = new JLabel("Enter the Event ID (This Can Be Found in the show events tab): ");
+                JTextField eventID = new JTextField(20);
+                
+                addParticipantFrame.add(eventIDInput);
+                addParticipantFrame.add(eventID);
+                addParticipantFrame.setLocationRelativeTo(null);
+                addParticipantFrame.setVisible(true);
+                
+                
+                
+                JLabel participanNameInput = new JLabel("Enter Participant name: ");
+                JTextField participanName = new JTextField(20);
+                
+                addParticipantFrame.add(participanNameInput);
+                addParticipantFrame.add(participanName);
+                addParticipantFrame.setLocationRelativeTo(null);
+                addParticipantFrame.setVisible(true);
+                
+                JLabel participanContactInfoInput = new JLabel("Enter Participant Contact Info");
+                JTextField participanContactInfo = new JTextField(20);
+                
+                addParticipantFrame.add(participanContactInfoInput);
+                addParticipantFrame.add(participanContactInfo);
+                addParticipantFrame.setLocationRelativeTo(null);
+                addParticipantFrame.setVisible(true);
+                
+                JButton buttonSubmit = new JButton("Submit");
+                buttonSubmit.addActionListener(new ActionListener() {
+                	public void actionPerformed(ActionEvent e) {
+                		try {
+                		if(manager.getEvents().size() > Integer.parseInt(eventID.getText())) {
+                			JFrame addParticipantFrame1 = new JFrame("Error");
+                			addParticipantFrame1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+                            addParticipantFrame1.setSize(400, 300);
+                            addParticipantFrame1.setLayout(new GridLayout(7,2,10,10));
+                			
+                			JLabel label = new JLabel("No such ID");
+                			
+                			addParticipantFrame1.add(label);
+                			addParticipantFrame1.setLocationRelativeTo(null);
+                            addParticipantFrame1.setVisible(true);
+                            
+                            JButton buttonSubmit = new JButton("Ok");
+                            
+                            buttonSubmit.addActionListener(new ActionListener() {
+                            	public void actionPerformed(ActionEvent e) {
+                            		addParticipantFrame1.dispose();
+                            	}
+                            });
+                            
+                            addParticipantFrame1.add(buttonSubmit);
+                            addParticipantFrame1.setLocationRelativeTo(null);
+                            addParticipantFrame1.setVisible(true);
+
+                        	
+                        } else if (manager.getEvents().get(Integer.parseInt(eventID.getText()) - 1).getCapacity() <= manager.getEvents().get(Integer.parseInt(eventID.getText()) - 1).getParticipants().size()) {
+                        	JFrame addParticipantFrame2 = new JFrame("Error");
+                        	addParticipantFrame2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+                            addParticipantFrame2.setSize(150, 200);
+                            addParticipantFrame2.setLayout(new GridLayout(7,2,10,10));
+                			
+                			JLabel label = new JLabel("Event Is Full");
+                			
+                			addParticipantFrame2.add(label);
+                			addParticipantFrame2.setLocationRelativeTo(null);
+                            addParticipantFrame2.setVisible(true);
+                            
+                            JButton buttonSubmit = new JButton("Ok");
+                            
+                            buttonSubmit.addActionListener(new ActionListener() {
+                            	public void actionPerformed(ActionEvent e) {
+                            		addParticipantFrame2.dispose();
+                            	}
+                            });
+                            
+                            addParticipantFrame2.add(buttonSubmit);
+                            addParticipantFrame2.setLocationRelativeTo(null);
+                            addParticipantFrame2.setVisible(true);
+                        } else {
+                        	_eventID = Integer.parseInt(eventID.getText());
+                    		_participantName = participanName.getText(); 
+                    		_participantContactInto = participanContactInfo.getText(); 
+                    		Participant p = new Participant(_participantName ,_participantContactInto );
+                    		manager.registerParticipantToEvent(_eventID , p);
+                    		System.out.println("Participan " + _participantName +" Has been added to [ " + eventName + " ]");
+                    		addParticipantFrame.dispose();
+                        }
+                		} catch (IndexOutOfBoundsException ee) {
+                			JFrame addParticipantFrame1 = new JFrame("Error");
+                			addParticipantFrame1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+                            addParticipantFrame1.setSize(400, 300);
+                            addParticipantFrame1.setLayout(new GridLayout(7,2,10,10));
+                			
+                			JLabel label = new JLabel("No such ID");
+                			
+                			addParticipantFrame1.add(label);
+                			addParticipantFrame1.setLocationRelativeTo(null);
+                            addParticipantFrame1.setVisible(true);
+                            
+                            JButton buttonSubmit = new JButton("Ok");
+                            
+                            buttonSubmit.addActionListener(new ActionListener() {
+                            	public void actionPerformed(ActionEvent e) {
+                            		addParticipantFrame1.dispose();
+                            	}
+                            });
+                            
+                            addParticipantFrame1.add(buttonSubmit);
+                            addParticipantFrame1.setLocationRelativeTo(null);
+                            addParticipantFrame1.setVisible(true);
+                		} catch (NumberFormatException ee) {
+                			JFrame addParticipantFrame1 = new JFrame("Error");
+                			addParticipantFrame1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+                            addParticipantFrame1.setSize(400, 300);
+                            addParticipantFrame1.setLayout(new GridLayout(7,2,10,10));
+                			
+                			JLabel label = new JLabel("Choose a number");
+                			
+                			addParticipantFrame1.add(label);
+                			addParticipantFrame1.setLocationRelativeTo(null);
+                            addParticipantFrame1.setVisible(true);
+                            
+                            JButton buttonSubmit = new JButton("Ok");
+                            
+                            buttonSubmit.addActionListener(new ActionListener() {
+                            	public void actionPerformed(ActionEvent e) {
+                            		addParticipantFrame1.dispose();
+                            	}
+                            });
+                            
+                            addParticipantFrame1.add(buttonSubmit);
+                            addParticipantFrame1.setLocationRelativeTo(null);
+                            addParticipantFrame1.setVisible(true);
+                		}
+                	}
+                });
+                
+                addParticipantFrame.add(buttonSubmit);
+                addParticipantFrame.setLocationRelativeTo(null);
+                addParticipantFrame.setVisible(true);
+                
+                
+        	}
+        });
+        
+        button6.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                final int[] currentPage = {0};
+                JFrame reportFrame = new JFrame("Registration Report");
+                reportFrame.setSize(500, 500);
+                reportFrame.setLayout(new BorderLayout());
+
+                JPanel listPanel = new JPanel(new GridLayout(5, 1));
+                JPanel navPanel = new JPanel();
+                
+                JButton prevBtn = new JButton("Previous");
+                JButton nextBtn = new JButton("Next");
+                JButton showMembersBtn = new JButton("Show Members");
+
+                ArrayList<RegistrationSystemOOP.Event> events = manager.getEvents();
+                listPanel.removeAll();
+                
+                for(int i = 0; i <= 4; i++) {
+                    if (i < events.size()) {
+                        listPanel.add(new JLabel("ID: " + events.get(i).getId() + " | " + events.get(i).getName() + " (" + events.get(i).getRegisteredCount() + " participants)"));
+                    } else {
+                        listPanel.add(new JLabel(" "));
+                    }
+                }
+
+                navPanel.add(nextBtn);
+                navPanel.add(showMembersBtn);
+
+                nextBtn.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        if ((currentPage[0] + 1) * 5 < events.size()) {
+                            currentPage[0]++;
+                            listPanel.removeAll();
+                            for(int i = currentPage[0] * 5; i < currentPage[0] * 5 + 5; i++) {
+                                if (i < events.size()) {
+                                     listPanel.add(new JLabel("ID: " + events.get(i).getId() + " | " + events.get(i).getName() + " (" + events.get(i).getRegisteredCount() + " participants)"));
+                                } else {
+                                    listPanel.add(new JLabel(" "));
+                                }
+                            }
+                            navPanel.removeAll();
+                            navPanel.add(prevBtn);
+                            navPanel.add(nextBtn);
+                            navPanel.add(showMembersBtn); 
+                            
+                            listPanel.revalidate();
+                            listPanel.repaint();
+                            navPanel.revalidate();
+                            navPanel.repaint();
+                        }
+                    }
+                });
+
+                prevBtn.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        if(currentPage[0] > 0) {
+                            currentPage[0]--;
+                            listPanel.removeAll();
+                            for(int i = currentPage[0] * 5; i < currentPage[0] * 5 + 5; i++) {
+                                if (i < events.size()) {
+                                     listPanel.add(new JLabel("ID: " + events.get(i).getId() + " | " + events.get(i).getName() + " (" + events.get(i).getRegisteredCount() + " participants)"));
+                                } else {
+                                    listPanel.add(new JLabel(" "));
+                                }
+                            }
+                            navPanel.removeAll();
+                            if(currentPage[0] == 0) {
+                                navPanel.add(nextBtn);
+                            } else {
+                                navPanel.add(prevBtn);
+                                navPanel.add(nextBtn);
+                            }
+                            navPanel.add(showMembersBtn);
+
+                            listPanel.revalidate();
+                            listPanel.repaint();
+                            navPanel.revalidate();
+                            navPanel.repaint();
+                        }
+                    }
+                });
+
+                showMembersBtn.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        JFrame idInputFrame = new JFrame("Select Event");
+                        idInputFrame.setSize(300, 150);
+                        idInputFrame.setLayout(new FlowLayout());
+                        
+                        JLabel lbl = new JLabel("Enter Event ID to view members:");
+                        JTextField idField = new JTextField(10);
+                        JButton searchBtn = new JButton("View");
+
+                        idInputFrame.add(lbl);
+                        idInputFrame.add(idField);
+                        idInputFrame.add(searchBtn);
+                        
+                        idInputFrame.setLocationRelativeTo(null);
+                        idInputFrame.setVisible(true);
+
+                        searchBtn.addActionListener(new ActionListener() {
+                            public void actionPerformed(ActionEvent e) {
+                                try {
+                                    int id = Integer.parseInt(idField.getText());
+                                    RegistrationSystemOOP.Event targetEvent = manager.findEventById(id);
+
+                                    if (targetEvent == null) {
+                                        JFrame err = new JFrame("Error");
+                                        err.setSize(200, 100);
+                                        err.add(new JLabel("  Event ID not found!"));
+                                        err.setVisible(true);
+                                    } else {
+                                        idInputFrame.dispose();
+                                        
+                                        JFrame membersFrame = new JFrame("Members for: " + targetEvent.getName());
+                                        membersFrame.setSize(400, 400);
+                                        membersFrame.setLayout(new BorderLayout());
+
+                                        JPanel memberListPanel = new JPanel();
+                                        memberListPanel.setLayout(new GridLayout(Math.max(1, targetEvent.getParticipants().size()), 1));
+                                        
+                                        ArrayList<Participant> parts = targetEvent.getParticipants();
+                                        
+                                        if (parts.isEmpty()) {
+                                            memberListPanel.add(new JLabel("   No participants registered yet."));
+                                        } else {
+                                            for (Participant p : parts) {
+                                                memberListPanel.add(new JLabel("   Name: " + p.getName() + " | Contact: " + p.getContactInfo()));
+                                            }
+                                        }
+                                        
+                                        JScrollPane scrollPane = new JScrollPane(memberListPanel);
+                                        membersFrame.add(scrollPane, BorderLayout.CENTER);
+                                        membersFrame.setLocationRelativeTo(null);
+                                        membersFrame.setVisible(true);
+                                    }
+
+                                } catch (NumberFormatException ex) {
+                                     JFrame err = new JFrame("Error");
+                                     err.setSize(200, 100);
+                                     err.add(new JLabel("  Please enter a valid number!"));
+                                     err.setVisible(true);
+                                }
+                            }
+                        });
+                    }
+                });
+
+                reportFrame.add(listPanel, BorderLayout.CENTER);
+                reportFrame.add(navPanel, BorderLayout.SOUTH);
+                reportFrame.setLocationRelativeTo(null);
+                reportFrame.setVisible(true);
+            }
+        });
         
 	}
 	
